850aef165041aca44211b84a832919b3e8ceb763
alannuarii <alannuariii@gmail.com>
2024-11-24 13:48:56 +0800

    Update

--- controllers/login.go
+++ controllers/login.go
@@ -38,7 +38,7 @@ func Login(c *gin.Context) {
 		"name": user.Name,
 		"email": user.Email,
 		"role": user.Role,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
+		"exp":  time.Now().AddDate(0, 1, 0).Unix(),
 	})
 
 	secretKey := os.Getenv("SECRET_KEY")
